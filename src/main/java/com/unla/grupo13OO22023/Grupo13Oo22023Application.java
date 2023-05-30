a5a4598dd737c903af30e2ccc8068ba0061825b8
Thlivon <thlivon@gmail.com>
2023-05-30 17:31:24 -0300

    first commit

---
+++ src/main/java/com/unla/grupo13OO22023/Grupo13Oo22023Application.java
@@ -0,0 +1,13 @@
+package com.unla.grupo13OO22023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo13Oo22023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo13Oo22023Application.class, args);
+	}
+
+}
