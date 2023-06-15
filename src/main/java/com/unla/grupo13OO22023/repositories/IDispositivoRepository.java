627e88ba70e5b7e2013f5c07e525c669b7bd7956
Homer <monsalvohomero@gmail.com>
2023-06-15 20:12:01 -0300

    Se agrego la dependencia JPA

--- src/main/java/com/unla/grupo13OO22023/repositories/IDispositivoRepository.java
+++ src/main/java/com/unla/grupo13OO22023/repositories/IDispositivoRepository.java
@@ -1,11 +1,15 @@
 package com.unla.grupo13OO22023.repositories;
 
+import java.io.Serializable;
+
 import com.unla.grupo13OO22023.entities.Dispositivo;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository("dispositivoRepository")
-public class IDispositivoRepository extends JpaRepository<Dispositivo, Serializable> {
+public interface IDispositivoRepository extends JpaRepository<Dispositivo, Serializable> {
 	public abstract Dispositivo findByIdDispositivo(int idDispositivo);
 	
 }
