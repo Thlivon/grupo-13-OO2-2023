4366a9c9df657e2f02ef9e399acf37227b0da4f6
Homer <monsalvohomero@gmail.com>
2023-06-21 00:20:50 -0300

    Se agrego el evento en el cambiarActivado

--- src/main/java/com/unla/grupo13OO22023/controllers/DispositivoController.java
+++ src/main/java/com/unla/grupo13OO22023/controllers/DispositivoController.java
@@ -13,9 +13,12 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
+import com.unla.grupo13OO22023.entities.Dispositivo;
+import com.unla.grupo13OO22023.entities.Evento;
 import com.unla.grupo13OO22023.entities.Habilitacion;
 import com.unla.grupo13OO22023.helpers.ViewRouteHelper;
 import com.unla.grupo13OO22023.services.IDispositivoService;
+import com.unla.grupo13OO22023.services.IEventoService;
 import com.unla.grupo13OO22023.services.IHabilitacionService;
 
 @Controller
@@ -28,6 +31,9 @@ public class DispositivoController {
 	@Autowired
 	@Qualifier("habilitacionService")
 	private IHabilitacionService habilitacionService;
+	@Autowired
+	@Qualifier("eventoService")
+	private IEventoService eventoService;
 
 	@GetMapping("")
 	public ModelAndView lista() {
@@ -72,6 +78,17 @@ public class DispositivoController {
 	@PostMapping("/cambiaractivado/{idDispositivo}")
 	public RedirectView cambiarActivado(@PathVariable("idDispositivo") int idDispositivo, RedirectAttributes redirectAttributes, Model model) {
 		boolean habilitacion = dispositivoService.findByIdDispositivo(idDispositivo).getHabilitado().isHabilitado();
+		//busco al dispositivo en la bdd
+		Dispositivo dispositivo = dispositivoService.findByIdDispositivo(idDispositivo);
+		//creo el evento
+		Evento evento = new Evento("Se modifico el estado a "+!dispositivo.isActivado(),dispositivo);
+		//establezco la relacion
+		dispositivo.getEventos().add(evento); 
+		//lo cargo en la bdd
+		eventoService.insertOrUpdate(evento); 
+		
+		
+		
 		if (habilitacion) {	//Si su tipo de dispositivo esta habilitado 
 			boolean aux = dispositivoService.findByIdDispositivo(idDispositivo).isActivado();
 			dispositivoService.cambiarActivado(idDispositivo, !aux);
